c89368901664e417d3f0d2b34315f243248c727b
sallylim <lim.sally20@gmail.com>
2022-05-15 23:38:56 -0700

    started creating sample question token

---
+++ src/Component/QnA/SampleQuestions.jsx
@@ -0,0 +1,18 @@
+import './SampleQuestions.css';
+
+function SampleQuestions() {
+
+    const sampleQuestions = [
+        "What does this code do?",
+        "What is the runtime complexity of this code?",
+
+    ]
+
+    return (
+        <div>
+        
+        </div>
+    );
+}
+
+export default SampleQuestions;
