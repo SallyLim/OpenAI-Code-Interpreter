35f1fecde3c74b66bf3c2bab2df1bfedee8e8e3a
sallylim <lim.sally20@gmail.com>
2022-05-22 03:40:00 -0700

    added decryption effect to landing page text

--- src/Component/LandingPage/LandingPage.jsx
+++ src/Component/LandingPage/LandingPage.jsx
@@ -2,12 +2,27 @@ import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 import Header from "../Header";
 import "./LandingPage.css";
 import { ReactComponent as Code } from "./Code.svg";
+import { useDencrypt } from "use-dencrypt-effect";
+import React from "react";
 
 function LandingPage() {
+  const value = "Understand code faster and easier";
+  const { result, dencrypt } = useDencrypt();
+
+  React.useEffect(() => {
+    let i = 0;
+
+    const action = setInterval(() => {
+      dencrypt(value);
+    }, 0);
+
+    return () => clearInterval(action);
+  }, []);
+
   return (
     <div className="landingPageContainer">
       <Header />
-      <h1 className="landingPageHeader">Understand code faster and easier</h1>
+      <h1 className="landingPageHeader">{result}</h1>
       <h3 className="landingPageSubheader">
         Ask OpenAI questions to get a better understanding of your code
       </h3>
