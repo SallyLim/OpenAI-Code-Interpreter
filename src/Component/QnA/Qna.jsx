9ef63903d208b2c86a8869c1c2e5125acae20e72
sallylim <lim.sally20@gmail.com>
2022-05-22 17:16:35 -0700

    changed model

--- src/Component/QnA/Qna.jsx
+++ src/Component/QnA/Qna.jsx
@@ -15,64 +15,42 @@ function Qna() {
   const [loading, setLoading] = useState(false);
   const [language, setLanguage] = useLocalStorage("language", "python");
 
-  //TODO: update the values and props
   const data = {
     prompt: code + "\n" + question,
     temperature: 0,
     max_tokens: 64,
   };
 
-  // const getAnswer = async () => {
-  //   setLoading(true);
-  //   const answer = await fetch(
-  //     "https://api.openai.com/v1/engines/text-davinci-001/completions",
-  //     {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         Authorization: `Bearer ${process.env.REACT_APP_OPENAI_KEY}`,
-  //       },
-  //       body: JSON.stringify(data),
-  //     }
-  //   );
-  //   const body = await answer.json();
-  //   let time = new Date().toLocaleString("en-US", {
-  //     timeZone: "America/Los_Angeles",
-  //   });
-
-  //   setLoading(false);
-  //   const copy = qnaList.map((x) => x);
-  //   let searchHistoryObject = {
-  //     code,
-  //     question,
-  //     answer: body.choices[0].text,
-  //     time,
-  //     language,
-  //   };
-  //   copy.push(searchHistoryObject);
-  //   setQnaList(copy);
-
-  //   setQuestion("");
-  // };
-
-  // TODO: eventually delete
-  const getAnswerTest = () => {
-    let testAnswer = "Test answer " + Math.random().toString();
-
+  const getAnswer = async () => {
+    setLoading(true);
+    const answer = await fetch(
+      "https://api.openai.com/v1/engines/text-davinci-001/completions",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.REACT_APP_OPENAI_KEY}`,
+        },
+        body: JSON.stringify(data),
+      }
+    );
+    const body = await answer.json();
     let time = new Date().toLocaleString("en-US", {
       timeZone: "America/Los_Angeles",
     });
 
+    setLoading(false);
     const copy = qnaList.map((x) => x);
     let searchHistoryObject = {
       code,
       question,
-      answer: testAnswer,
+      answer: body.choices[0].text,
       time,
       language,
     };
     copy.push(searchHistoryObject);
     setQnaList(copy);
+
     setQuestion("");
   };
 
@@ -91,16 +69,9 @@ function Qna() {
             onChange={(v) => setQuestion(v.target.value)}
           />
           <SampleQuestions setQuestion={setQuestion} />
-          <button
-            className="submitButton"
-            type="submit"
-            onClick={getAnswerTest}
-          >
+          <button className="submitButton" type="submit" onClick={getAnswer}>
             Submit
           </button>
-          {/* <button className="submitButton" type="submit" onClick={getAnswer}>
-            Submit
-          </button> */}
           <PrevQnaLog qnaList={qnaList} loading={loading} />
         </div>
         <div className="flexDiv2">
